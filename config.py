4e413ade2788447d3b41c326ceba039f9339ce3b
Sam <affum3331@gmail.com>
2025-07-29 23:27:10 +0000

    Add configuration settings and implement TransformerClassifier with positional encoding

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    "d_model": 128,
+    "num_heads": 4,
+    "d_ff": 256,
+    "dropout": 0.1,
+    "num_classes": 4,
+    "epochs": 5,
+    "batch_size": 64,
+    "lr": 1e-3,
+    "max_len": 128,
+    "use_dyt": False
+}
--- models/encoder.py
+++ models/encoder.py
@@ -2,11 +2,12 @@ from models.encoder_layer import TransformerEncoderLayer
 import torch.nn as nn
 from utils.DyT import DyT
 from utils.residual import NormType
+from utils.postional_encoding import PositionalEncoding
 
 
 class TransformerEncoder(nn.Module):
     def __init__(self, num_layers, d_model, num_heads,d_ff, dropout,norm_type):
-        super.__init__()
+        super().__init__()
 
         self.layers = nn.ModuleList([
             TransformerEncoderLayer(d_model, num_heads, d_ff, dropout, norm_type)
@@ -19,3 +20,20 @@ class TransformerEncoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return x
+    
+
+class TransformerClassifier(nn.Module):
+    def __init__(self, vocab_size, config):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, config["d_model"])
+        self.pe = PositionalEncoding(config["d_model"], config["max_len"])
+        norm_type = NormType.LAYER_NORM if not config["use_dyt"] else NormType.DYT
+        self.encoder = TransformerEncoder(config["num_layers"], config["d_model"], config["num_heads"],
+                                          config["d_ff"], config["dropout"], norm_type)
+        self.classifier = nn.Linear(config["d_model"], config["num_classes"])
+    
+    def forward(self, x):
+        x = self.embedding(x) + self.pe(x)
+        x = self.encoder(x)
+        return self.classifier(x[:,0])
+
